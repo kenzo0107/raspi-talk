965d1eb8ba39b088bee8d809a6201b23ccd66985
Kenzo Tanaka <kenzo.tanaka0107@gmail.com>
2023-05-12 23:54:29 +0900

    gtts で 日本語テキストを mp3 ファイルとして読み上げる
    テキストを gtts (Google Cloud Text-to-Speech) ライブラリで
    音声ファイル mp3 に変換し
    USB スピーカーから再生させます。
    
    音声ファイル再生後、ビープ音を再生させます。
    
    後々、会話できる様にする為の準備です。

---
+++ speech.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import tempfile
+
+from gtts import gTTS
+from pydub import AudioSegment
+from pydub.playback import play
+
+tmpdir = tempfile.mkdtemp()
+tmpfile = os.path.join(tmpdir, 'out.mp3')
+
+
+def speechja(stext):
+    tts = gTTS(stext, lang="ja")
+    tts.save(tmpfile)
+
+    sound = AudioSegment.from_mp3(tmpfile)
+    play(sound)
+
+
+if __name__ == "__main__":
+    args = sys.argv
+    if len(args) == 2:
+        msg = args[1]
+    else:
+        msg = "はい、お元気ですか？"
+    print(msg)
+    speechja(msg)
+
+    beep = AudioSegment.from_mp3("/home/pi/button.mp3")
+    play(beep)
