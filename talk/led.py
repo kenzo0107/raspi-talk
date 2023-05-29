b5e8855e5eba23d25ab5f4a50d676628e545178e
Kenzo Tanaka <kenzo.tanaka0107@gmail.com>
2023-05-29 22:11:26 +0900

    LED 点灯・消灯 制御
    motion sensor が検知し、
    会話のこちらのターンになった際に点灯、
    話終わりに消灯することを想定し、追加した機能です。

---
+++ talk/led.py
@@ -0,0 +1,35 @@
+import sys
+from time import sleep
+
+import RPi.GPIO as GPIO
+
+
+class LED:
+    def __init__(self, led_pin):
+        self.led_pin = led_pin
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.led_pin, GPIO.OUT)
+
+    def turn_on(self):
+        GPIO.output(self.led_pin, GPIO.HIGH)
+
+    def turn_out(self):
+        GPIO.output(self.led_pin, GPIO.LOW)
+
+    def blink(self):
+        while True:
+            try:
+                led.turn_on()
+                sleep(0.5)
+                led.turn_out()
+                sleep(0.5)
+            except KeyboardInterrupt:
+                GPIO.cleanup()
+                sys.exit()
+
+
+if __name__ == '__main__':
+    led_pin = 16
+    led = LED(led_pin)
+    led.blink()
