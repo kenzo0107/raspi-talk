84bd0c3c1be74c6a84248050984aebddc155eb0d
Kenzo Tanaka <kenzo.tanaka0107@gmail.com>
2023-05-29 22:25:33 +0900

    USB マイクからの音声を録音し任意の音声ファイルに保存する機能追加
    OpenAI との会話で [USB マイク](https://www.amazon.co.jp/gp/product/B09QMC7QWN/?&_encoding=UTF8&tag=kenzo0107-22&linkCode=ur2&linkId=3c646b43b03a3a97bfe559ca50a1443e&camp=247&creative=1211) を通じ
    音声ファイルを保存できる様にします。
    
    以降の処理で音声ファイルをテキスト変換する処理を実施する想定です。
    
    ## 2秒 音声がない場合に録音を停止する
    
    ```
    class AudioRecorder:
        def __init__(self, chunk=4096, channel=1, rate=44100, threshold=1200):
            ...
            self.silent_chunk = 2 * rate / chunk
    
        def start_record(self):
            ...
            silent_chunks = 0
            while True:
                data = stream.read(self.chunk)
                rms = audioop.rms(data, 2)
                if rms < self.threshold:
                    silent_chunks += 1
                    if silent_chunks > self.silent_chunk:
                        break
    ```
    
    スマートデバイスの様に話終わりを検知したく
    2秒黙ったら録音終了して OpenAI に話した内容を送信し返信を待つような仕組みを想定しています。

---
+++ talk/audio_recorder.py
@@ -0,0 +1,61 @@
+import audioop
+import wave
+
+import pyaudio
+
+# カード番号
+# RPi で `arecord -l` 実行し検知したUSB マイクのハードウェアのカード番号を設定する
+CARD_NUM = 0
+
+
+class AudioRecorder:
+    def __init__(self, chunk=4096, channel=1, rate=44100, threshold=1200):
+        self.chunk = chunk
+        self.format = pyaudio.paInt16
+        self.channel = channel
+        self.rate = rate
+        self.threshold = threshold
+        self.silent_chunk = 2 * rate / chunk
+        self.frames = []
+        self.audio = pyaudio.PyAudio()
+
+    def start_record(self):
+        stream = self.audio.open(
+            format=self.format,
+            channels=self.channel,
+            rate=self.rate,
+            input_device_index=CARD_NUM,
+            input=True,
+            output=False,
+            frames_per_buffer=self.chunk,
+        )
+
+        silent_chunks = 0
+        while True:
+            data = stream.read(self.chunk)
+            rms = audioop.rms(data, 2)
+            if rms < self.threshold:
+                silent_chunks += 1
+                if silent_chunks > self.silent_chunk:
+                    break
+            else:
+                silent_chunks = 0
+            self.frames.append(data)
+
+        stream.stop_stream()
+        stream.close()
+        self.audio.terminate()
+
+    def save_recording(self, file_path):
+        wf = wave.open(file_path, 'wb')
+        wf.setnchannels(self.channel)
+        wf.setsampwidth(self.audio.get_sample_size(self.format))
+        wf.setframerate(self.rate)
+        wf.writeframes(b''.join(self.frames))
+        wf.close()
+
+
+if __name__ == '__main__':
+    recorder = AudioRecorder()
+    recorder.start_record()
+    recorder.save_recording('record.wav')
